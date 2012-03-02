a641c168bd7e86e467cb92638543268454ce7c2f
Timo Sand <timo.sand@kapsi.fi>
2012-03-02 17:46:20 +0200

    Part 1b not done yet

--- Homework 1/part1.rb
+++ Homework 1/part1.rb
@@ -5,3 +5,12 @@ end
 puts palindrome?("A man, a plan, a canal -- Panama")
 puts palindrome?("Madam, I'm Adam!")
 puts palindrome?("Abracadabra")
+
+def count_words(string)
+	count_hash = {}
+	string.downcase.scan(/\w+/).each { |x| count_hash[x] += 1 }
+	count_hash
+end
+
+puts count_words("A man, a plan, a canal -- Panama")
+puts count_words("Doo dee doo bee doo")
