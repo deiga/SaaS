54c8e60a79bf5b7be74913d2195a2adb83e20ad4
Timo Sand <timo.sand@kapsi.fi>
2012-03-03 14:46:01 +0200

    Part 3 done

--- Homework 1/part3.rb
+++ Homework 1/part3.rb
@@ -1,5 +1,11 @@
 def combine_anagrams(words)
-
+	anagrams = Hash.new
+	words.each do |word|
+		anagram_key = word.downcase.chars.sort.join
+		anagrams[anagram_key] ||= Array.new
+		anagrams[anagram_key] << word
+	end
+	anagrams.values
 end
 
 puts combine_anagrams(['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream']).inspect
