2483c2338e98b2b5f90abd7e326f16182084e16f
Timo Sand <timo.sand@kapsi.fi>
2012-03-03 15:12:26 +0200

    Finished Part 5

--- Homework 1/part5.rb
+++ Homework 1/part5.rb
@@ -1,13 +1,13 @@
 class Class
 	def attr_accessor_with_history(attr_name)
 		attr_name = attr_name.to_s
-		attr_reader attr_reader
+		attr_reader attr_name
 		attr_reader attr_name+"_history"
 		class_eval %Q{
 			def #{attr_name}=(new_attr)
 				@#{attr_name}_history ||= Array.new << @#{attr_name}
 				@#{attr_name}_history << new_attr
-				super
+				@#{attr_name} = new_attr
 			end
 		}
 	end
@@ -16,9 +16,3 @@ end
 class Foo
 	attr_accessor_with_history :bar
 end
-
-f = Foo.new
-f.bar = 3
-f.bar = :wowzo
-f.bar = 'boo!'
-puts f.bar_history
