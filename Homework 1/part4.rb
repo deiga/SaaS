58c3d854f2b27212662dbae4aee8b25c3ceadc97
Timo Sand <timo.sand@kapsi.fi>
2012-03-03 14:51:16 +0200

    Began working on part 4a

---
+++ Homework 1/part4.rb
@@ -0,0 +1,18 @@
+class Dessert
+
+	attr_accessor :name, :calories
+	def initialize(name, calories)
+		@name = name
+		@calories = calories
+	end
+
+	def healthy?
+		@calories < 200
+	end
+
+	def delicious?
+		true
+	end
+	
+	
+end
