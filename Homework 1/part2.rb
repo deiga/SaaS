d181527873ce5466508ed9c3826bc6d88239b1cb
Timo Sand <timo.sand@kapsi.fi>
2012-03-03 14:24:29 +0200

    Finished Part 2

--- Homework 1/part2.rb
+++ Homework 1/part2.rb
@@ -34,23 +34,34 @@ def rps_game_winner(game)
 	raise WrongNumberOfPlayersError unless game.length == 2
 	raise NoSuchStrategyError unless ['R', 'P', 'S'].include? game[0][1] and ['R', 'P', 'S'].include? game[1][1]
 	if game[0][1] == game[1][1]
-		game[0].inspect
+		game[0]
 	elsif game[0][1].rps_win? game[1][1]
-		game[0].inspect
+		game[0]
 	elsif game[1][1].rps_win? game[0][1]
-		game[1].inspect
+		game[1]
 	end
 end
 
-puts rps_game_winner([["Armando", "P"], ["Dave", "S"]])
-puts rps_game_winner([["Armando", "P"], ["Dave", "P"]])
+#puts rps_game_winner([["Armando", "P"], ["Dave", "S"]])[0]
+#puts rps_game_winner([ ["Dave", "P"], ["Armando", "S"] ])[0]
+#puts rps_game_winner([ ["Allen", "P"], ["Richard", "P"] ])[0]
+
 
 def rps_tournament_winner(tournament)
+	rps_tournament(tournament).flatten
+end
+
+def rps_tournament(tournament)
 	winners = Array.new
-	1.upto tournament.flatten.length {
-		winners << rps_game_winner(tournament.flatten.pop(2))
+	flat_tournament = tournament.flatten.reverse
+	1.upto(flat_tournament.length / 4) { 
+		players = Array.new
+		players << flat_tournament.pop(2).reverse
+		players << flat_tournament.pop(2).reverse
+		winners << rps_game_winner(players) 
 	}
+	winners = rps_tournament_winner(winners) unless winners.length == 1
 	winners
 end
 
-puts rps_tournament_winner( [[[["Armando", "P"], ["Dave", "S"]], [["Richard", "R"], ["Michael", "S"]], ], [[["Allen", "S"], ["Omer", "P"]], [[ "David E.", "R"], ["Richard X.", "P"]]]])
+#puts rps_tournament_winner( [[[["Armando", "P"], ["Dave", "S"]], [["Richard", "R"], ["Michael", "S"]], ], [[["Allen", "S"], ["Omer", "P"]], [[ "David E.", "R"], ["Richard X.", "P"]]]]).inspect
