713a53000bae1392a13f8f4f70c588fbab591a42
Timo Sand <timo.sand@kapsi.fi>
2012-03-08 17:12:39 +0200

    Fixed Part 1

--- Homework 2/part1.rb
+++ Homework 2/part1.rb
@@ -41,7 +41,7 @@ puts "Abracadabra".palindrome?
 
 module Enumerable
   def palindrome?()
-    self.to_s.gsub(/\W/,'').downcase == self.to_s.gsub(/\W/,'').downcase.reverse
+    self.to_a.to_s.gsub(/\W/,'').downcase == self.to_a.to_s.gsub(/\W/,'').downcase.reverse
   end
 end
 
